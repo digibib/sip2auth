385e7f86fa949fc73bae07f375d6a28d73c3e3d0
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-06-18 08:16:32 +0200

    fixes indentation

--- sip2auth.rb
+++ sip2auth.rb
@@ -5,43 +5,43 @@ require "socket"
 require "time"
 
 class Client
-	def initialize(host, port)
-		# hostname (or ip adress) and port of the sip2-server
-		@host, @port = host, port
-	end
+  def initialize(host, port)
+    # hostname (or ip adress) and port of the sip2-server
+    @host, @port = host, port
+  end
 
-	def send_message(msg)
-		connection do |socket|
-			socket.puts(msg)
-			result = socket.gets("\r") # read socket until carriage return
-			return result
-		end
-	end
+  def send_message(msg)
+    connection do |socket|
+      socket.puts(msg)
+      result = socket.gets("\r") # read socket until carriage return
+      return result
+    end
+  end
 
-	def connection
-		socket = Socket.new Socket::AF_INET, Socket::SOCK_STREAM, 0
-		socket.connect Socket.pack_sockaddr_in @port, @host
-		yield(socket)
-	ensure
-		socket.close
-	end
+  def connection
+    socket = Socket.new Socket::AF_INET, Socket::SOCK_STREAM, 0
+    socket.connect Socket.pack_sockaddr_in @port, @host
+    yield(socket)
+  ensure
+    socket.close
+  end
 end
 
 def appendChecksum(msg)
-	check = 0
-	msg.each_char { |m| check += m.ord }
-	check += "\0".ord
-	check = (check ^ 0xFFFF) + 1
+  check = 0
+  msg.each_char { |m| check += m.ord }
+  check += "\0".ord
+  check = (check ^ 0xFFFF) + 1
 
-	checksum = "%4.4X" % check
-	return msg + checksum
+  checksum = "%4.4X" % check
+  return msg + checksum
 end
 
 def formMessage(cardnr, pin)
-	code = "63"
-	language = "001"
-	timestamp = Time.now.strftime("%Y%m%d    %H%M%S")
-	summary = " " * 10
+  code = "63"
+  language = "001"
+  timestamp = Time.now.strftime("%Y%m%d    %H%M%S")
+  summary = " " * 10
   msg = code + language + timestamp + summary + "AO|AA" + cardnr + "|AC|AD" + pin + "|AY1AZ"
   return msg
 end
@@ -49,8 +49,8 @@ end
 # -------------------------
 
 if ARGV.length != 4
-	puts "Usage: sip2auth.rb <HOST> <PORT> <CARD NUMBER> <CARD PIN>"
-	exit
+  puts "Usage: sip2auth.rb <HOST> <PORT> <CARD NUMBER> <CARD PIN>"
+  exit
 end
 
 host, port, cardnr, pin = ARGV[0], ARGV[1], ARGV[2], ARGV[3]
